a4426c552ccf315ff75e01b08a5b73896abcd888
Johann Fotsing <johann.fotsing@outlook.com>
2022-06-18 16:44:01 +0200

    First draft of the Arcade structure representing an arcade machine

---
+++ src/Arcade_private.h
@@ -0,0 +1,26 @@
+/**
+ * @file Arcade_private.h
+ * @author Johann Fotsing (johann.fotsing@outlook.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-06-18
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#ifndef ARCADE_PRIVATE
+#define ARCADE_PRIVATE
+
+#include "Arcade.h"
+#include "Cpu8080.h"
+#include <SDL2/SDL.h>
+
+struct s_Arcade
+{
+    Cpu8080* cpu;
+    SDL_Window* window;
+    SDL_Renderer* rd;
+};
+
+#endif // ARCADE_PRIVATE
