1b81f2aac68f457533b3417d2ee0dfbf642e0051
Johann Fotsing <johann.fotsing@outlook.com>
2024-10-01 21:24:07 +0200

    Use single invaders binary (seems to be the correct one).

--- src/main/arcade/arcade.c
+++ src/main/arcade/arcade.c
@@ -54,21 +54,13 @@ void arcade_init_cpu(Arcade* arcade, const char* rom_folder_path)
     /// Load SpaceInvaders ROM files
     size_t len = strlen(rom_folder_path);
     size_t copy_len = rom_folder_path[len-1] == '/' ? len - 1 : len;
-    
-    const char* first_rom_name = "/invaders.h";
+    const char* first_rom_name = "/invaders";
     size_t rom_filename_len = strlen(first_rom_name);
     size_t file_path_len = copy_len + rom_filename_len;
     char* rom_file_path = (char*) malloc(file_path_len + 1);
     rom_file_path = strcpy(rom_file_path, rom_folder_path);
     rom_file_path = strcat(rom_file_path, first_rom_name);
-    
     cpu8080_load_rom(arcade->cpu, rom_file_path, 0x0000);
-    rom_file_path[file_path_len-1] = 'g';
-    cpu8080_load_rom(arcade->cpu, rom_file_path, 0x0800);
-    rom_file_path[file_path_len-1] = 'f';
-    cpu8080_load_rom(arcade->cpu, rom_file_path, 0x1000);
-    rom_file_path[file_path_len-1] = 'e';
-    cpu8080_load_rom(arcade->cpu, rom_file_path, 0x1800);
 
     free(rom_file_path);
 
