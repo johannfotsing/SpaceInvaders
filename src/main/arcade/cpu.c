db4a7ff10f048ac927a90bb2d6c3f8ee739bbc3f
Johann Fotsing <johann.fotsing@outlook.com>
2024-10-04 17:01:34 +0200

    CPU clock emulation (8080 runs @2Mhz) .

--- src/main/arcade/cpu.c
+++ src/main/arcade/cpu.c
@@ -22,10 +22,31 @@ void arcade_init_cpu(Arcade* arcade, const char* rom_file_path)
 int arcade_emulate_cpu(void* a)
 {
     Arcade* arcade = (Arcade*) a;
+
+    Uint32 emulation_period_ms = 2;
+    Uint64 nb_cycles_per_ms = 2e+6 / 1e+3;
+    Uint64 last_emulation_time = SDL_GetTicks64();
+
+    // 
+    Uint64 nb_cycles_emulated = 0;
+    Uint64 max_cycles_to_emulate;
+    int nb_op_cycles;
     
     while (arcade->running)
     {
-        cpu8080_emulate(arcade->cpu, NULL);
+        if (SDL_GetTicks64() - last_emulation_time > emulation_period_ms)
+        {
+            max_cycles_to_emulate = (SDL_GetTicks64() - last_emulation_time) * nb_cycles_per_ms;
+            max_cycles_to_emulate = max_cycles_to_emulate > 4000 ? 4000 * 1.5: max_cycles_to_emulate;
+            last_emulation_time = SDL_GetTicks64();
+            nb_cycles_emulated = 0;
+            while (nb_cycles_emulated < max_cycles_to_emulate)
+            {
+                cpu8080_emulate(arcade->cpu, &nb_op_cycles);
+                nb_cycles_emulated += nb_op_cycles;
+            }
+        }
+        SDL_Delay(1);
     }
 
     return 0;
